cd7c9c5570422b4002959de97f8aafd6ec584586
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-05-16 12:00:08 +0300

    refactorizare funcite in main

--- src/main/java/com/codecool/old_book_auction/App.java
+++ src/main/java/com/codecool/old_book_auction/App.java
@@ -10,12 +10,15 @@ import java.util.List;
 
 public class App {
 
+    private static final Random random = new Random();
+
     public static Topic getRandomTopic() {
         Topic[] topics = Topic.values();
         Random random = new Random();
         int randomIndex = random.nextInt(topics.length);
         return topics[randomIndex];
     }
+
 
     private static List<Book> createBooks (
             int bookCount,
@@ -54,6 +57,21 @@ public class App {
         }
         return bidders;
     }
+
+    public static List<Bidder> filterBuyers(List<Bidder> bidders,Book book) {
+        List<Bidder> buyers = new ArrayList<>();
+        for (Bidder bidder : bidders) {
+            if (bidder.interested(book)) {
+                System.out.println(bidder.getName() + " " + bidder.getFavourite() + " " + Arrays.toString(bidder.getInterested()));
+                System.out.println("--------");
+                if (bidder.canBid(book, book.getPrice())) {
+                    buyers.add(bidder);
+                }
+                System.out.println(buyers);
+            }
+        }
+        return buyers;
+    }
     public static void main(String[] args) {
         final int bookCount = 20;
         final int minPrice = 100;
@@ -66,25 +84,28 @@ public class App {
         List<Book> books = createBooks(bookCount, minPrice, maxPrice);
         List<Bidder> bidders = createBidders(bidderCount, minimumCapital, maximumCapital);
         List<Book> soldBooks = new ArrayList<>();
-        for (int i = 0; i < books.size(); i++) {
-            System.out.println(books.get(i).getTitle());
-            System.out.println(books.get(i).getTopic());
-            System.out.println(books.get(i).getPrice());
-            List<Bidder> buyers = new ArrayList<>();
-            for (int j = 0; j < bidders.size(); j++) {
-                if (bidders.get(j).interested(books.get(i))){
-                    System.out.println(bidders.get(j).getName() + " " + bidders.get(j).getFavourite() + " " + Arrays.toString(bidders.get(j).getInterested()));
-                    System.out.println("--------");
-                    if (bidders.get(j).canBid(books.get(i), books.get(i).getPrice())) {
-                        buyers.add(bidders.get(j));
-                    }
-                }
-            }
-            if (buyers.size() == 1){
-                soldBooks.add(books.get(i));
+        for (Book book : books) {
+            System.out.println(book.getTitle());
+            System.out.println(book.getTopic());
+            System.out.println(book.getPrice());
+
+            List<Bidder> buyers =  filterBuyers(bidders,book);
+            if (buyers.size() == 1) {
+                soldBooks.add(book);
+            }else if(buyers.size() > 1){
+               int currentBidderIndex =  random.nextInt(0,buyers.size());
+               System.out.println(bidders.get(currentBidderIndex).getName() + " bids " + book.getPrice());
+//                try {
+//                    Thread.sleep(3000);
+//                } catch (InterruptedException e) {
+//                    // Handle the interrupted exception
+//                    e.printStackTrace();
+//                }
             }
 //            System.out.println(buyers);
         }
-        System.out.println(soldBooks.size());
+
+
+     //   System.out.println(soldBooks.size());
     }
 }
